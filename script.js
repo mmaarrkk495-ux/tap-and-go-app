717bcf40af5c07c3e868c1c977aa379d7bca031c
mmaarrkk495-ux <mmaarrkk495@gmail.com>
2025-08-13 15:53:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function calibrateAndListen() {
     setTimeout(() => {
         clearInterval(calibrationInterval);
         const ambientNoiseLevel = samples.reduce((a, b) => a + b, 0) / samples.length;
-        dynamicKnockThreshold = (ambientNoiseLevel * 1.16) + 16; 
+        dynamicKnockThreshold = (ambientNoiseLevel * 2.5) + 25; 
         updateUIMode('listening');
         detectKnock();
     }, calibrationTime);
